d29188d66c2d07047859e8ab68108cebc2a05718
MariannaSarkisian <spn.mjj@gmail.com>
2024-09-20 23:57:04 +0300

    add git hw5

--- Desktop/hw_5srapy/hw_5srapy/spiders/__init__.py
+++ Desktop/hw_5srapy/hw_5srapy/spiders/__init__.py
@@ -1,4 +1,4 @@
 # This package will contain the spiders of your Scrapy project
-#
+#"https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
